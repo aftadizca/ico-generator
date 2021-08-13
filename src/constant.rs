a48acc4a9b0e89f370b280125c0d07c1ed46a1f4
aftadizca <aftadizca@gmail.com>
2021-08-13 18:25:43 +0700

    working search folder

---
+++ src/constant.rs
@@ -0,0 +1,8 @@
+pub const IMG_SIZE: u32 = 256;
+
+pub mod MIDDLE_IMG {
+    pub const W: u32 = 149;
+    pub const H: u32 = 211;
+    pub const X: u32 = 31;
+    pub const Y: u32 = 16;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,69 @@
 #![allow(unused_imports)]
+#![allow(unused_variables)]
 #![allow(dead_code)]
 
+extern crate glob;
 extern crate image;
 extern crate rust_embed;
+mod constant;
+use constant::MIDDLE_IMG as MIDDLE;
 
+//rust-embed
 use rust_embed::RustEmbed;
 #[derive(RustEmbed)]
 #[folder = "src/img/"]
-// #[prefix = "/"]
 struct Asset;
 
+use glob::glob;
 use image::io::Reader as ImageReader;
 use image::{GenericImage, GenericImageView, ImageBuffer, ImageDecoder, RgbImage};
 use std::boxed::Box;
 use std::error::Error;
+use std::path::Path;
+use std::vec::Vec;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let img1_asset = Asset::get("test2.jpg").unwrap();
-    let img2_asset = Asset::get("test.png").unwrap();
-    // println!("{:?}", img_path.data.as_ref());
-    // for file in Asset::iter() {
-    //     println!("{}", file.as_ref());
-    // }
+    for i in search_anime_folder("")? {
+        println!("{}", i)
+    }
+    // process_image("src/img/test.jpg", "test2.ico")?;
+    Ok(())
+}
 
-    // let img = image::open("src/img/test2.jpg")?;
-    // let img2 = image::open("src/img/test.png")?;
-    let img = image::load_from_memory(img1_asset.data.as_ref())?;
-    let img2 = image::load_from_memory(img2_asset.data.as_ref())?;
+fn search_anime_folder(folder: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let folder = "/mnt/d/KOLEKSI/NEWANIME/*/";
+    let mut result = Vec::new();
+    for entry in glob(folder).unwrap() {
+        match entry {
+            Ok(p) => {
+                let path_ico = Path::new(p.as_path().to_str().unwrap()).join("a.ico");
+                let path_jpg = Path::new(p.as_path().to_str().unwrap()).join("icon.jpg");
+                if path_ico.exists() && !path_jpg.exists() && !p.as_path().ends_with("1. new") {
+                    result.push(p.as_path().to_str().unwrap().to_string());
+                }
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    Ok(result)
+}
 
-    let mut img = img.resize_exact(512, 512, image::imageops::FilterType::Lanczos3);
-    let img2 = img2.resize_exact(512, 512, image::imageops::FilterType::Lanczos3);
-
-    image::imageops::overlay(&mut img, &img2, 0, 0);
-
-    img.save_with_format("test2.png", image::ImageFormat::Png)?;
-    println!("w:{} h:{}", img.width(), img.height());
+fn process_image(path: &str, out_path: &str) -> Result<(), Box<dyn Error>> {
+    //read image from asset
+    let top_asset = Asset::get("top.png").unwrap();
+    let bottom_asset = Asset::get("bottom.png").unwrap();
+    let middle_asset = image::open(path)?;
+    //load image
+    let top_asset = image::load_from_memory(top_asset.data.as_ref())?;
+    let mut bottom_asset = image::load_from_memory(bottom_asset.data.as_ref())?;
+    //resizing middle image
+    let middle_asset =
+        middle_asset.resize_exact(MIDDLE::W, MIDDLE::H, image::imageops::FilterType::Lanczos3);
+    //stacking bottom & middle img
+    image::imageops::overlay(&mut bottom_asset, &middle_asset, MIDDLE::X, MIDDLE::Y);
+    //stacking bottom & top img
+    image::imageops::overlay(&mut bottom_asset, &top_asset, 0, 0);
+    //save image
+    bottom_asset.save_with_format(out_path, image::ImageFormat::Ico)?;
     Ok(())
 }
