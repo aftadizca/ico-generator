184bd47ca40e645292276095b8cb464364c23769
aftadizca <aftadizca@gmail.com>
2021-08-14 23:52:05 +0700

    working good

--- src/constant.rs
+++ src/constant.rs
@@ -1,8 +1,21 @@
 pub const IMG_SIZE: u32 = 256;
 
-pub mod MIDDLE_IMG {
+pub mod middle_img {
     pub const W: u32 = 149;
     pub const H: u32 = 211;
     pub const X: u32 = 31;
     pub const Y: u32 = 16;
+}
+
+pub mod anilist {
+    pub const QUERY: &str = "
+        query ($search: String){ 
+            Media(search: $search, type:ANIME) { 
+                id 
+                coverImage {
+                    extraLarge
+                }
+            }
+        }
+        ";
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-extern crate glob;
-extern crate image;
-extern crate rust_embed;
 mod constant;
 
 //rust-embed
@@ -13,23 +10,24 @@ use rust_embed::RustEmbed;
 #[folder = "src/img/"]
 struct Asset;
 
-use constant::MIDDLE_IMG as MIDDLE;
+use constant::anilist;
+use constant::middle_img as MIDDLE;
 use glob::glob;
-use hyper::Client;
-use image::io::Reader as ImageReader;
 use image::{GenericImage, GenericImageView, ImageBuffer, ImageDecoder, RgbImage};
+use reqwest::Client;
+use serde_json::json;
 use std::boxed::Box;
 use std::error::Error;
 use std::path::Path;
-use std::vec::Vec;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    create_anime_folder("")?;
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    // get_img_from_anilist("AnoHana")?;
+    create_anime_folder("");
     // process_image("src/img/test.jpg", "test2.ico")?;
     Ok(())
 }
 
-fn create_anime_folder(folder: &str) -> Result<(), Box<dyn Error>> {
+fn create_anime_folder(folder: &str) {
     let folder = "/mnt/d/KOLEKSI/NEWANIME/*/";
     for entry in glob(folder).unwrap() {
         match entry {
@@ -38,12 +36,16 @@ fn create_anime_folder(folder: &str) -> Result<(), Box<dyn Error>> {
                 let path_jpg = Path::new(p.as_path().to_str().unwrap()).join("icon.jpg");
                 if path_ico.exists() && !path_jpg.exists() && !p.as_path().ends_with("1. new") {
                     println!("{}", p.as_path().file_name().unwrap().to_str().unwrap());
+                    get_img_from_anilist(
+                        p.as_path().file_name().unwrap().to_str().unwrap(),
+                        p.as_path().to_str().unwrap(),
+                    )
+                    .unwrap()
                 }
             }
             Err(e) => println!("{:?}", e),
         }
     }
-    Ok(())
 }
 
 fn process_image(path: &str, out_path: &str) -> Result<(), Box<dyn Error>> {
@@ -67,8 +69,33 @@ fn process_image(path: &str, out_path: &str) -> Result<(), Box<dyn Error>> {
 }
 
 #[tokio::main]
-async fn get_img_from_anilist() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // This is where we will setup our HTTP client requests.
-
+async fn get_img_from_anilist(
+    title: &str,
+    output_path: &str,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let client = Client::new();
+    let json = json!({"query": anilist::QUERY, "variables": {"search": title}});
+    // Make HTTP post request
+    let resp = client
+        .post("https://graphql.anilist.co/")
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .body(json.to_string())
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await;
+    // Get json
+    let result: serde_json::Value = serde_json::from_str(&resp.unwrap()).unwrap();
+    let url_img = result["data"]["Media"]["coverImage"]["extraLarge"].to_owned();
+    println!("{}", url_img.as_str().unwrap());
+    let resp = reqwest::get(url_img.as_str().unwrap()).await?;
+    let mut content = resp.bytes().await?;
+    let img = image::load_from_memory(&mut content)?;
+    img.save_with_format(
+        format!("{}/icon.jpg", output_path),
+        image::ImageFormat::Jpeg,
+    )?;
     Ok(())
 }
